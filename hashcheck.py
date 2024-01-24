76998ffa5b14a277dac2a771901c9d077f48a968
aenos <46544513+Aen-OS@users.noreply.github.com>
2024-01-24 23:12:01 +0000

    Update hashcheck.py

--- hashcheck.py
+++ hashcheck.py
@@ -1,4 +1,5 @@
 import os
+import sys
 """
 1. ask for input1 and input2
 2. compare input1 to input2
@@ -14,16 +15,10 @@ MD5, SHA1, SHA256, SHA512
 def hca():
     #input sequence
     hash1 = input("Input first hash:\n")
-    if hash1 == "":
-        print("INVALID OR MISSING INPUT")
-    else:
-        pass
+    if hash1 == "": print("INVALID OR MISSING INPUT"); sys.exit()
 
     hash2 = input("Input second hash:\n")
-    if hash2 == "":
-        print("INVALID OR MISSING INPUT")
-    else:
-        pass
+    if hash2 == "": print("INVALID OR MISSING INPUT"); sys.exit()
 
     # get rid of whitespaces
     hash1 = hash1.strip()
@@ -34,12 +29,8 @@ def hca():
     
     #outputs accordingly if hashes match or not
     if hash1 == hash2:
-        print(hash1)
-        print(hash2)
-        print("VALID MATCH")
+        print(hash1); print(hash2); print("VALID MATCH")
     else:
-        print(hash1)
-        print(hash2)
-        print("INVALID MATCH")
+        print(hash1); print(hash2); print("INVALID MATCH")
 
 hca()
